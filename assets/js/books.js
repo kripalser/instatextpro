c155c67e1a0abd5fd6032228a0f7bcfd04c8e7b0
Pavel Krimont <kripalser@yandex.com>
2019-02-25 22:16:13 +0600

    Add typographer

--- assets/js/books.js
+++ assets/js/books.js
@@ -21,8 +21,8 @@ $(function () {
       var authorsCount  = $('.author-inputs').children().length;
 
       $container.find('.book-author-primary').html(itp.slide._data.bookAuthorRu);
-      $container.find('.book-title-primary').html(itp.slide._data.bookTitleRu);
-      $container.find('.book-series-primary').html(itp.slide._data.bookSeriesRu);
+      $container.find('.book-title-primary').html(itp.slide._data.bookTitleRu).typographer();
+      $container.find('.book-series-primary').html(itp.slide._data.bookSeriesRu).typographer();
       // $container.find('.book-author-secondary').html(itp.slide._data.bookAuthorOrig);
       // $container.find('.book-title-secondary').html(itp.slide._data.bookTitleOrig);
       // $container.find('.book-series-secondary').html(itp.slide._data.bookSeriesOrig);
@@ -33,32 +33,6 @@ $(function () {
           $container.find('.book-author-primary').append(',<br>' + itp.slide._data['bookAuthorRu' + i]);
         }
       }
-
-/*      var bookTitleRu   = itp.slide._data.bookTitleRu,
-          bookAuthorRu  = itp.slide._data.bookAuthorRu,
-          bookTitleOrig   = itp.slide._data.bookTitleRu,
-          bookAuthorOrig  = itp.slide._data.bookTitleRu,
-          bookYear      = itp.slide._data.bookTitleRu;*/
-
-/*
-      if (itp.slide._data[2] !== undefined && itp.slide._data[3] !== undefined) {
-        bookTitleEn   = itp.slide._data[2];
-        bookAuthorEn  = itp.slide._data[3];
-        bookYear  = itp.slide._data[4];
-      } else {
-        bookYear  = itp.slide._data[2];
-      }
-*/
-
-      /*var bookSlideContent = '<div class="book-slide-content">' +
-                             '<div class="book-slide-author-primary">' + bookAuthorRu + '</div>' +
-                             '<div class="book-slide-title-primary">' + bookTitleRu + '</div>' +
-                             '<div class="book-slide-title-secondary">' + bookTitleEn + '</div>' +
-                             '<div class="book-slide-author-secondary">' + bookAuthorEn + '</div>' +
-                             '<div class="book-slide-year">' + bookYear + '</div>' +
-                             '</div>'; // Todo: create a markup in HTML then clone it
-
-      $(itp.document._target).append($(html).addClass('book-slide').append(bookSlideContent).attr('id', 'slide1'));*/
 
       itp.image.generate();
 
