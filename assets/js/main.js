39fc1ea3ab717b0f58944d1a0e5e2c09ce77a6c1
Pavel Krimont <kripalser@yandex.com>
2019-06-20 15:05:54 +0600

    Change name, replace external scripts and Bootstrap with local copies, make books the default and only behaviour

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,136 +1,89 @@
-/*var ingredients = {
- "ingredients": [
- {
- "name": "Сельдь",
- "amount": 1,
- "measure": "штука"
- }, {
- "name": "Сливочное масло",
- "amount": 200,
- "measure": "г"
- }, {
- "name": "Яйцо",
- "amount": 2,
- "measure": "штуки"
- }, {
- "name": "Укроп",
- "amount": 30,
- "measure": "г"
- }
- ]
- };*/
-
 $(function () {
 
   var itp = {};
 
   itp.document = {
 
-    _target: '.container'
+    _target       : '.container',
+    _sourceClass  : 'book-container',
+    _sourceId     : 'bookContainer'
 
   };
 
-  /*itp.data = {
-
-   _ingredients: '<ol><li>Сельдь</li><li>Сливочное масло</li><li>Яйцо</li><li>Укроп</li></ol>',
-   _steps: [
-   '1. Яйцо отварить, почистить. Размять вилкой.',
-   '2. Добавить к яйцу размягченное сливочное масло, сельдь рубленную, лук репчатый и горчицу (последняя по желанию).',
-   '3. Провернуть все ингредиенты в блендере вместе с укропом — до кремообразного состояния.',
-   '4. Приятного аппетита!'
-   ]
-
-   };*/
-
   itp.slide = {
 
-    _current: 1,
     _class: 'slide',
-    _data: [], // Todo: store data as an object
+    _data: {},
 
-    generate: function (bookSlide) {
+    generate: function () {
 
-      var html = '<div class="' + itp.slide._class + '"></div>';
+      var $container    = $('#' + itp.document._sourceId);
+      var authorsCount  = $('.author-inputs').children().length;
 
-      if (bookSlide) {
+      $container.find('.book-author-primary').html(itp.slide._data.bookAuthorRu);
+      $container.find('.book-title-primary').html(itp.slide.addNbsp(itp.slide._data.bookTitleRu));
+      $container.find('.book-series-primary').html(itp.slide.addNbsp(itp.slide._data.bookSeriesRu));
+      // $container.find('.book-author-secondary').html(itp.slide._data.bookAuthorOrig);
+      // $container.find('.book-title-secondary').html(itp.slide._data.bookTitleOrig);
+      // $container.find('.book-series-secondary').html(itp.slide._data.bookSeriesOrig);
+      $container.find('.book-year').html(itp.slide._data.bookYear);
 
-        var bookTitleRu   = itp.slide._data[0],
-            bookAuthorRu  = itp.slide._data[1],
-            bookTitleEn   = '',
-            bookAuthorEn  = '',
-            bookYear      = '';
-
-        if (itp.slide._data[2] !== undefined && itp.slide._data[3] !== undefined) {
-          bookTitleEn   = itp.slide._data[2];
-          bookAuthorEn  = itp.slide._data[3];
-          bookYear  = itp.slide._data[4];
-        } else {
-          bookYear  = itp.slide._data[2];
+      for (var i = 2; i <= authorsCount; i++) {
+        if (itp.slide._data['bookAuthorRu' + i]) {
+          $container.find('.book-author-primary').append(',<br>' + itp.slide._data['bookAuthorRu' + i]);
         }
-
-        var bookSlideContent = '<div class="book-slide-content">' +
-                                 '<div class="book-slide-title-primary">' + bookTitleRu + '</div>' +
-                                 '<div class="book-slide-author-primary">' + bookAuthorRu + '</div>' +
-                                 '<div class="book-slide-title-secondary">' + bookTitleEn + '</div>' +
-                                 '<div class="book-slide-author-secondary">' + bookAuthorEn + '</div>' +
-                                 '<div class="book-slide-year">' + bookYear + '</div>' +
-                               '</div>'; // Todo: create a markup in HTML then clone it
-
-        $(itp.document._target).append($(html).addClass('book-slide').append(bookSlideContent).attr('id', 'slide1'));
-
-        itp.image.generate(bookSlide);
-
-      } else {
-
-        $.each(itp.slide._data, function (index, value) {
-
-          $(itp.document._target).append($(html).append(value).attr('id', 'slide' + (index + 1)));
-
-          if ((index + 1) === itp.slide._data.length) {
-            // console.log('Done!');
-            itp.image.generate();
-          }
-
-        });
-
       }
 
+      itp.image.generate();
+
+    },
+
+    addNbsp: function (text) {
+      /*! Typographer - v0.1.0 - 2015-08-18
+       * https://github.com/sakharstudio/typographer
+       * Copyright (c) 2015 Loginov Yura; Licensed MIT */
+      var prepositions = ["в", "во", "без", "до", "из", "к", "ко", "на", "по", "о", "от", "при", "с", "у", "не", "за", "над", "для", "об", "под", "про", "и", "а", "но", "да", "или", "ли", "бы", "то", "что", "как", "я", "он", "мы", "они", "ни", "же", "вы", "им"],
+        nbsp = '&nbsp;',
+        prepositionsOne = prepositions.map(function (item) {
+          return ' ' + item + ' ';
+        }),
+        prepositionsTwo = prepositions.map(function (item) {
+          return '&nbsp;' + item + ' ';
+        }),
+        regex = new RegExp(prepositionsOne.join('|'), 'gi'),
+        regex2 = new RegExp(prepositionsTwo.join('|'), 'gi'),
+        replacement = function (str) {
+          return str.slice(0, -1) + nbsp;
+        };
+
+      return text.replace(regex, replacement).replace(regex2, replacement);
     }
 
   };
 
   itp.image = {
 
-    generate: function (bookSlide) {
+    generate: function () {
 
-      var element = itp.slide._class + itp.slide._current;
+      var element = itp.document._sourceId;
       var elementId = '#' + element;
       var $element = $(elementId)[0];
 
       html2canvas($element, {
 
         onclone: function (clonedDocument) {
-          $(clonedDocument).find(elementId).css('display', 'flex');
+          // $(clonedDocument).find(elementId).css('display', 'block');
+          $(clonedDocument).find(elementId).removeClass('d-none');
         }
 
       }).then(function (canvas) {
 
         var img = new Image();
-        // var imgSrc = canvas.toDataURL();
-        // img.src = imgSrc;
-        img.src = canvas.toDataURL();
-        img.className = 'slide-img';
-        /*var link = document.createElement('a');
-         link.appendChild(img);
-         link.href = imgSrc;
-         link.setAttribute('download', element + '.png');
-         $('.container').append($(link));*/
-        $('.container').append($(img));
 
-        if (!bookSlide && itp.slide._current < itp.slide._data.length) {
-          itp.slide._current++;
-          itp.image.generate();
-        }
+        img.src = canvas.toDataURL();
+        img.className = 'book-img';
+
+        $('.container').append($(img));
 
       });
 
@@ -142,42 +95,52 @@ $(function () {
 
     var $form = $('form');
 
-    /*itp.slide._data.push('<ol><li>Сельдь</li><li>Сливочное масло</li><li>Яйцо</li><li>Укроп</li></ol>');
-     itp.slide._data.push('1. Яйцо отварить, почистить. Размять вилкой.');
-     itp.slide._data.push('2. Добавить к яйцу размягченное сливочное масло, сельдь рубленную, лук репчатый и горчицу (последняя по желанию).');
-     itp.slide._data.push('3. Провернуть все ингредиенты в блендере вместе с укропом — до кремообразного состояния.');
-     itp.slide._data.push('4. Приятного аппетита!');*/
-
-    // Todo: move the function into method
-    // Todo: parse ingredients and steps as separate arrays/groups/whatever?
-
-    function showValues(bookSlide) {
-      // var dataArray = $('form').serializeArray();
-      // console.log(dataArray);
-
-      var fields = $form.serializeArray();
-      $form.hide();
-      // $('#results').empty();
-
-      jQuery.each(fields, function (i, field) {
-        // $('#results').append(field.name + ': ' + field.value + ', ');
-        // console.log(field.name + ': ' + field.value + ', ');
-
-        if (field.value !== '') {
-          itp.slide._data.push(field.value);
-        }
-
-        if (i + 1 === fields.length) {
-          itp.slide.generate(bookSlide);
-        }
-
-      });
-
-    }
-
     $form.on('submit', function (e) {
       e.preventDefault();
-      showValues(true);
+      // itp.slide._data = $form.serializeArray();
+      // $form.hide();
+      $form.addClass('d-none');
+      $('.btn-edit').removeClass('d-none');
+
+      var formData = $form.serializeArray();
+
+      $(formData).each(function (index, obj) {
+        itp.slide._data[obj.name] = obj.value;
+      });
+
+      // console.log(itp.slide._data);
+
+      itp.slide.generate();
+
+    });
+
+    $('.btn-edit').on('click', function (e) {
+      $('.book-img').remove();
+      $('form').removeClass('d-none');
+      $(e.target).addClass('d-none');
+    });
+
+    $('.btn-add-author').on('click', function (e) {
+      e.preventDefault();
+
+      var $button = $(this),
+        $container  =  $('.author-inputs'),
+        count       = $container.children().length + 1,
+        $formGroup  = $('#formGroupAuthor').clone(),
+        $label      = $formGroup.find('label'),
+        $input      = $formGroup.find('input'),
+        $text       = $formGroup.find('.form-text');
+
+      $button.addClass('mt-1');
+      $formGroup.attr('id', $formGroup.attr('id') + count);
+      $label.attr('for', $label.attr('for') + count);
+      $label.html('Ещё один автор');
+      $input.val('');
+      $input.attr('id', $input.attr('id') + count);
+      $input.attr('name', $input.attr('name') + count);
+      $input.removeAttr('required');
+      $text.remove();
+      $formGroup.appendTo($container);
     });
 
     // itp.slide.generate();
